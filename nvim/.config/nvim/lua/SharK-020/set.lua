f4bbf5169a437187d604d54ad8e4d7f3bdcdbb79
SharK-020 <dev.kunalshar0@gmail.com>
2025-01-24 13:23:09 +0530

    added treesitter

--- nvim/.config/nvim/lua/SharK-020/set.lua
+++ nvim/.config/nvim/lua/SharK-020/set.lua
@@ -1,10 +1,9 @@
---- Line Number Set
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
---- Tabs Set
 vim.opt.tabstop = 4
-vim.opt.softtabstor = 4
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
@@ -12,17 +11,21 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
 vim.opt.hlsearch = false
-vim.opt.insearch = true
+vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.signolumn = "yes"
-vim.opt.isfname: append "@-@"
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
-vim.g.maplleader = " "
