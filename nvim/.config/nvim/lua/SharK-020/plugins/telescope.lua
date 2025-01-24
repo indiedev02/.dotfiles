77256eeeba496daaeb24473a1cce69cbed99bca5
SharK-020 <dev.kunalshar0@gmail.com>
2025-01-24 17:29:06 +0530

    lsp config

--- nvim/.config/nvim/lua/SharK-020/plugins/telescope.lua
+++ nvim/.config/nvim/lua/SharK-020/plugins/telescope.lua
@@ -1,25 +1,28 @@
 return {	
-	'nvim-telescope/telescope.nvim', 
-	tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		require('telescope').setup({})
+    'nvim-telescope/telescope.nvim', 
 
-		local builtin = require("telescope.builtin")
+    tag = '0.1.8',
 
-		vim.keymap.set('n','<leader>pf',builtin.find_files,{})
-		vim.keymap.set('n','<C-p>',builtin.git_files,{})
-		vim.keymap.set('n','<leader>pws',function()
-			local word = vim.fn.expand('<cword>')
-			builtin.grep_string({search=word})
-		end)
-		vim.keymap.set('n','<leader>pWs',function()
-			local word = fim.fn.expand('<cWORD>')
-			builtin.grep_string({search=word})
-		end)
-		vim.keymap.set('n','<leader>ps',function()
-			builtin.grep_string({search=vim.fn.input('Grep > ')}) 
-		end)
-	end
+    dependencies = { 'nvim-lua/plenary.nvim' },
+
+    config = function()
+        require('telescope').setup({})
+
+        local builtin = require("telescope.builtin")
+
+        vim.keymap.set('n','<leader>pf',builtin.find_files,{})
+        vim.keymap.set('n','<C-p>',builtin.git_files,{})
+        vim.keymap.set('n','<leader>pws',function()
+            local word = vim.fn.expand('<cword>')
+            builtin.grep_string({search=word})
+        end)
+        vim.keymap.set('n','<leader>pWs',function()
+            local word = vim.fn.expand('<cWORD>')
+            builtin.grep_string({search=word})
+        end)
+        vim.keymap.set('n','<leader>ps',function()
+            builtin.grep_string({search=vim.fn.input('Grep > ')}) 
+        end)
+    end
 
 }
--- nvim/.config/nvim/lua/SharK-020/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/SharK-020/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+
     build = ":TSUpdate",
+
     config = function()
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
@@ -16,19 +18,11 @@ return {
             -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
             auto_install = true,
 
-            indent = {
-                enable = true
-            },
-
             highlight = {
                 -- `false` will disable the whole extension
                 enable = true,
-                disable = function(lang, buf)
-                    if lang == "html" then
-                        print("disabled")
-                        return true
-                    end
 
+                disable = function(lang, buf)
                     local max_filesize = 100 * 1024 -- 100 KB
                     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
                     if ok and stats and stats.size > max_filesize then
@@ -48,16 +42,5 @@ return {
                 additional_vim_regex_highlighting = { "markdown" },
             },
         })
-
-        local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        treesitter_parser_config.templ = {
-            install_info = {
-                url = "https://github.com/vrischmann/tree-sitter-templ.git",
-                files = {"src/parser.c", "src/scanner.c"},
-                branch = "master",
-            },
-        }
-
-        vim.treesitter.language.register("templ", "templ")
     end
 }
