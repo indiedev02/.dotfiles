53664ca5c32a673ebbd4de5766eac182a2a896e7
SharK-020 <dev.kunalshar0@gmail.com>
2025-01-24 13:09:27 +0530

    added vim config sets

--- nvim/.config/nvim/lua/SharK-020/init.lua
+++ nvim/.config/nvim/lua/SharK-020/init.lua
@@ -1,3 +1,4 @@
+require("SharK-020.set")
 require("SharK-020.remap")
-require("SharK-020.lazy")
+require("SharK-020.lazy") 
 
---
+++ nvim/.config/nvim/lua/SharK-020/set.lua
@@ -0,0 +1,28 @@
+--- Line Number Set
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+--- Tabs Set
+vim.opt.tabstop = 4
+vim.opt.softtabstor = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.insearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signolumn = "yes"
+vim.opt.isfname: append "@-@"
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.g.maplleader = " "
