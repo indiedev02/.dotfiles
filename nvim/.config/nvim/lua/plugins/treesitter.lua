5b4d984c3c39ae1e4b2c94589cdfed7e5fa95e3f
SharK-020 <dev.kunalshar0@gmail.com>
2025-02-02 02:31:10 +0530

    reset config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,52 +1,25 @@
-return {
-    "nvim-treesitter/nvim-treesitter",
-
-    build = ":TSUpdate",
-
-    config = function()
-        require "nvim-treesitter.configs".setup({
-            ensure_installed = {
-                "javascript", "typescript", "lua", "vim", "vimdoc",
-                "jsdoc", "bash", "rust", "python", "go", "tsx"
-            },
-
-            sync_install = false,
-
-            auto_install = true,
-
-            indent = {
-                enable = true,
-            },
-
-            highlight = {
-                enable = true,
-                disable = function(lang, buf)
-                    local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        vim.notify(
-                            "File larger than 100KB, Treesitter disabled for performance",
-                            vim.log.levels.WARN,
-                            { title = "Treesitter" }
-                        )
-                        return true
-                    end
-                    return false
-                end,
-
-                additional_vim_regex_highlighting = { "markdown" },
-            }
-        })
-
-        local parser_config = require"nvim-treesitter.parsers".get_parser_configs()
-        parser_config.templ = {
-            install_info = {
-                url = "https://github.com/vrischmann/tree-sitter-templ.git",
-                files = { "src/parser.c", "src/scanner.c" },
-                branch = "master",
-            },
-        }
-
-        vim.treesitter.language.register("templ", "templ")
-    end,
+return { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    opts = {
+        ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'javascript', 'typescript' , 'python' , 'go' , 'rust' },
+        -- Autoinstall languages that are not installed
+        auto_install = true,
+        highlight = {
+            enable = true,
+            -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+            --  If you are experiencing weird indenting issues, add the language to
+            --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+            additional_vim_regex_highlighting = { 'ruby' },
+        },
+        indent = { enable = true, disable = { 'ruby' } },
+    },
+    -- There are additional nvim-treesitter modules that you can use to interact
+    -- with nvim-treesitter. You should go explore a few and see what interests you:
+    --
+    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 }
