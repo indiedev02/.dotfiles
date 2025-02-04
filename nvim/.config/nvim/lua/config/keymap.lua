9b918bb2de88cfa49a844c3dceff4b2a35c01793
SharK-020 <dev.kunalshar0@gmail.com>
2025-02-05 00:09:44 +0530

    added tmux nav

--- nvim/.config/nvim/lua/config/keymap.lua
+++ nvim/.config/nvim/lua/config/keymap.lua
@@ -2,6 +2,10 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>")
 
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
 --TIP: Disable arrow keys in normal mode
 vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
