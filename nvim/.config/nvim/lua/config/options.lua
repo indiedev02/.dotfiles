c062b9cc17c7674d6c5b1f5fa0aecedb0ed11209
SharK-020 <dev.kunalshar0@gmail.com>
2025-02-07 13:09:31 +0530

    lzay nvim

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,4 @@
+vim.g.snacks_animate = false
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
---
+++ nvim/.config/nvim/lua/plugins/catppucin.lua
@@ -0,0 +1,9 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }, -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
