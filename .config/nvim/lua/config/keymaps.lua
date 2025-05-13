44cab7f1e295bc757b5334db689059c774f72ef7
indiedev02 <dev.kunalshar0@gmail.com>
2025-05-13 16:33:15 +0530

    tmux added

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opt = { noremap = true, silent = true }
+
+-- New tab
+keymap.set("n", "te", ":tabedit<Return>", opt)
+keymap.set("n", "<tab>", ":tabnext<Return>", opt)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opt)
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Diagnostics
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.jump({ count = 1, float = true })
+end)
